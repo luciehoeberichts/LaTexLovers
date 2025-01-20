16ca25f52460d4e6de2a5c0b725dc16c6b48c4f3
Zosia <zosia.pronczuk@gmail.com>
2025-01-20 17:56:03 +0100

    dictionary for real

--- script.py
+++ script.py
@@ -1,27 +1,28 @@
 import json
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-data_foot = []
+data_foot_size = []
 
 for item in alphabet:
     with open (f'../People/{item}_people.json', encoding = 'utf-8') as people_file:
         content_people = json.load(people_file)
     for person in content_people:
-        if 'ontology/shoeSize' in person:
-             data_foot.append(person['ontology/shoeSize'])
-
-print(len(data_foot))
-
-# dictionary_people = eval(content_people)
-# with open('results.json', 'w', encoding='utf-8') as file:
-#     json.dump(dictionary_people, file, indent = 4)
+        if 'ontology/shoeSize' in person and 'ontology/birthPlace_label' in person:            
+            foot_dictionary = {"shoeSize": person['ontology/shoeSize'], 
+                                "place": str(person['ontology/birthPlace_label'])
+                                }
+            data_foot_size.append(foot_dictionary)
+             
 
 
 
-# shoesize = []
 
-# for element in dictionary_people:
-#     if item['ontology/shoeSize'] == '(UK) 9 (US) 11 (EU) 41-42':
-# #        shoesize.append(item)
 
-# print(shoesize)
+
+
+
+
+with open('results.json', 'w', encoding='utf-8') as file:
+    json.dump(data_foot_size, file, indent = 4)
+
+
