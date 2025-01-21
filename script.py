3b5056a41659eae54a9e4001889ee017467d079e
Zosia <zosia.pronczuk@gmail.com>
2025-01-21 12:38:17 +0100

    foot sizes converted

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import json
 import csv 
 from csv import DictWriter
+import re
+
+foot_size_detector = re.compile(r'([1-9][0-9]*(\.[0-9])?)')
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 data_foot_size = []
@@ -9,9 +12,20 @@ for item in alphabet:
     with open (f'./People/{item}_people.json', encoding = 'utf-8') as people_file:
         content_people = json.load(people_file)
     for person in content_people:
-        if 'ontology/shoeSize' in person and 'ontology/birthPlace_label' in person:            
+        if 'ontology/shoeSize' in person and 'ontology/birthPlace_label' in person: 
+            shoe_sizes = foot_size_detector.findall(str(person['ontology/shoeSize']))
+
+            largest_size = 0
+            for shoe_size, fraction in shoe_sizes:
+                shoe_size = float(shoe_size)
+                if shoe_size > largest_size:
+                    largest_size = shoe_size
+
+            if largest_size > 20:
+                largest_size = ((largest_size - 2) / 1.27) - 21.5
+
             foot_dictionary = {
-                "shoeSize": person['ontology/shoeSize'], 
+                "shoeSize": largest_size, 
                 "place": person['ontology/birthPlace_label'][0] if type(person['ontology/birthPlace_label']) is list 
                             else person['ontology/birthPlace_label']
             }
@@ -21,7 +35,7 @@ for item in alphabet:
 
 
 
-with open('data_foot_size.csv', 'w', encoding='utf-8') as file:
+with open('data_foot_size.csv', 'w', encoding='utf-8', newline='') as file:
     writer = csv.DictWriter(file, ['shoeSize', 'place'])
     writer.writeheader()
     writer.writerows(data_foot_size)
